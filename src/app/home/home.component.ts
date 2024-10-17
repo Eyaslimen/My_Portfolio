0696aec7626d0f1942c57ad4791bd89d8516acf4
Eyaslimen <slimen.eya1@gmail.com>
2024-10-17 17:04:18 +0200

    first3page_done

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { HelloComponent } from "../hello/hello.component";
+import { AboutComponent } from "../about/about.component";
+import { SkillsComponent } from "../skills/skills.component";
+import { ProjectsComponent } from "../projects/projects.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [HelloComponent, AboutComponent, SkillsComponent, ProjectsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
